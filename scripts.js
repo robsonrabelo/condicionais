38fe0563e9482219326b96947c7a6a19db3628cb
Robson Rabelo <141787805+robsonrabelo@users.noreply.github.com>
2024-05-21 08:30:58 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var idade = prompt("Informe a sua idade?")
+
+if (idade < 18) {
+    alert("Você é menor de idade!")
+} else if (idade == 18) {
+    alert("Você acabou de atingir a maior idade!")
+} else {
+    alert("Você é maior de idade!")
+}
